fa37b166ad7dff9bb065cdcdcbbac65fbaa53723
Lutow <118168487+Lutow@users.noreply.github.com>
2025-05-08 13:40:44 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.send('API Server Running');
+});
+
+
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Hello from Node backend!' });
+});
+
+app.get('/api/login', (req, res) => {
+    const { email, password } = req.body;
+    res.json({ message: 'Login success!' });
+})
+
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+});
