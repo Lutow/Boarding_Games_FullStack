18da95bd52d731e6cbd4622995a183b639b745b9
Lutow <118168487+Lutow@users.noreply.github.com>
2025-05-08 13:44:22 +0100

    Add files via upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,1 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Log in.vue';
+
+const routes = [{ path: '/login', name: 'Login', component: Login }];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
