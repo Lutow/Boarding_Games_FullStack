e45fe2cf7d8d4ee2afd99698f4a687d1f27eca6c
Lutow <118168487+Lutow@users.noreply.github.com>
2025-05-12 22:48:16 +0100

    Add files via upload

---
+++ DB INSERTION.sql
@@ -0,0 +1,119 @@
+-- Insert data into Game table (with IDs 1-30)
+INSERT INTO Game (game_id, game_image, name, year_published, min_players, max_players, playingtime, theme, description, game_mechanic, purchase_link) VALUES
+(1, 'https://cf.geekdo-images.com/S3ybV1LAp-8SnHIXLLjVqA__micro/img/S4tXI3Yo7BtqmBoKINLLVUFsaJ0=/fit-in/64x64/filters:strip_icc()/pic1534148.jpg', 'Pandemic', '2008', 2, 4.0, 45, 'Medical', 'In Pandemic, several virulent diseases have broken out simultaneously all over the world! The players are disease-fighting specialists whose mission is to treat disease hotspots while researching cures for each of four plagues before they get out of hand.', 'Cooperative Game', '/boardgame/30549/pandemic'),
+(2, 'https://cf.geekdo-images.com/okM0dq_bEXnbyQTOvHfwRA__micro/img/VfLoKzfk3xj26ArmDu55qZ4sysw=/fit-in/64x64/filters:strip_icc()/pic6544250.png', 'Carcassonne', '2000', 2, 5.0, 45, 'City Building', 'Carcassonne is a tile-placement game in which the players draw and place a tile with a piece of southern French landscape on it.', 'Tile Placement', '/boardgame/822/carcassonne'),
+(3, 'https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw__micro/img/LA4OvGfQ_TXQ-2mhaIFZp2ITWpc=/fit-in/64x64/filters:strip_icc()/pic2419375.jpg', 'Catan', '1995', 3, 4.0, 120, 'Economic', 'In CATAN, players try to be the dominant force on the island of Catan by building settlements, cities, and roads.', 'Trading', '/boardgame/13/catan'),
+(4, 'https://cf.geekdo-images.com/RvFVTEpnbb4NM7k0IF8V7A__micro/img/9glsOs7zoTbkVpfDt5SHWJm-kRA=/fit-in/64x64/filters:strip_icc()/pic860217.jpg', '7 Wonders', '2010', 2, 7.0, 30, 'Civilization', 'You are the leader of one of the 7 great cities of the Ancient World. Gather resources, develop commercial routes, and affirm your military supremacy.', 'Drafting', '/boardgame/68448/7-wonders'),
+(5, 'https://cf.geekdo-images.com/j6iQpZ4XkemZP07HNCODBA__micro/img/PVxqHWOLTb3n-4xe62LJadr_M0I=/fit-in/64x64/filters:strip_icc()/pic394356.jpg', 'Dominion', '2008', 2, 4.0, 30, 'Card Game', 'You are a monarch, like your parents before you, a ruler of a small pleasant kingdom of rivers and evergreens.', 'Deck Building', '/boardgame/36218/dominion'),
+(6, 'https://cf.geekdo-images.com/ZWJg0dCdrWHxVnc0eFXK8w__micro/img/79tvkijvf1wFKrKz-FWt77XdIbw=/fit-in/64x64/filters:strip_icc()/pic38668.jpg', 'Ticket to Ride', '2004', 2, 5.0, 60, 'Trains', 'With elegantly simple gameplay, Ticket to Ride can be learned in under 15 minutes.', 'Route Building', '/boardgame/9209/ticket-ride'),
+(7, 'https://cf.geekdo-images.com/F_KDEu0GjdClml8N7c8Imw__micro/img/w38Q9MZMQK80Att5GcODbCGufDk=/fit-in/64x64/filters:strip_icc()/pic2582929.jpg', 'Codenames', '2015', 2, 8.0, 15, 'Party Game', 'Codenames is an easy party game to solve puzzles.', 'Team-Based Game', '/boardgame/178900/codenames'),
+(8, 'https://cf.geekdo-images.com/wg9oOLcsKvDesSUdZQ4rxw__micro/img/LUkXZhd1TO80eCiXMD3-KfnzA6k=/fit-in/64x64/filters:strip_icc()/pic3536616.jpg', 'Terraforming Mars', '2016', 1, 5.0, 120, 'Science Fiction', 'In the 2400s, mankind begins to terraform the planet Mars.', 'Engine Building', '/boardgame/167791/terraforming-mars'),
+(9, 'https://cf.geekdo-images.com/WzNs1mA_o22ZWTR8fkLP2g__micro/img/xh3isprMbt_FCg9vs3w_ifv-JXY=/fit-in/64x64/filters:strip_icc()/pic3376065.jpg', '7 Wonders Duel', '2015', 2, 2.0, 30, 'Ancient', 'In many ways 7 Wonders Duel resembles its parent game 7 Wonders.', 'Card Drafting', '/boardgame/173346/7-wonders-duel'),
+(10, 'https://cf.geekdo-images.com/dDDo2Hexl80ucK1IlqTk-g__micro/img/SZgGqufNqaW8BCFT29wkYPaRXOE=/fit-in/64x64/filters:strip_icc()/pic831744.jpg', 'Agricola', '2007', 1, 5.0, 150, 'Farming', 'In Agricola, you''re a farmer in a wooden shack with your spouse and little else.', 'Worker Placement', '/boardgame/31260/agricola'),
+(11, 'https://cf.geekdo-images.com/QFiIRd2kimaMqTyWsX0aUg__micro/img/ggNIAA1GEadqvcY7Mvwb9_VEybU=/fit-in/64x64/filters:strip_icc()/pic158548.jpg', 'Puerto Rico', '2002', 3, 5.0, 150, 'Economic', 'In Puerto Rico, players assume the roles of colonial governors on the island of Puerto Rico.', 'Role Selection', '/boardgame/3076/puerto-rico'),
+(12, 'https://cf.geekdo-images.com/rwOMxx4q5yuElIvo-1-OFw__micro/img/VaWkmRffgWdhbynm-GUM4anrlic=/fit-in/64x64/filters:strip_icc()/pic1904079.jpg', 'Splendor', '2014', 2, 4.0, 30, 'Economic', 'Splendor is a game of chip-collecting and card development.', 'Engine Building', '/boardgame/148228/splendor'),
+(13, 'https://cf.geekdo-images.com/7k_nOxpO9OGIjhLq2BUZdA__micro/img/ETplc22GdhlN5u0mppcDxSP8LVk=/fit-in/64x64/filters:strip_icc()/pic3163924.jpg', 'Scythe', '2016', 1, 5.0, 115, 'Science Fiction', 'It is a time of unrest in 1920s Europa.', 'Area Control', '/boardgame/169786/scythe'),
+(14, 'https://cf.geekdo-images.com/T1ltXwapFUtghS9A7_tf4g__micro/img/Qiu0VH-5VxmCUkISsvN5-dRlrKw=/fit-in/64x64/filters:strip_icc()/pic1401448.jpg', 'Love Letter', '2012', 2, 4.0, 20, 'Card Game', 'All of the eligible young men (and many of the not-so-young) seek to woo the princess of Tempest.', 'Deduction', '/boardgame/129622/love-letter'),
+(15, 'https://cf.geekdo-images.com/0K1AOciqlMVUWFPLTJSiww__micro/img/fNo9_FY6sdu7szEouS2v9IVjHfI=/fit-in/64x64/filters:strip_icc()/pic66668.jpg', 'Ticket to Ride: Europe', '2005', 2, 5.0, 60, 'Trains', 'Ticket to Ride: Europe takes you on a new train adventure across Europe.', 'Route Building', '/boardgame/14996/ticket-ride-europe'),
+(16, 'https://cf.geekdo-images.com/tz19PfklMdAdjxV9WArraA__micro/img/N7xRslgynWzB3aObMBI9F1yKfQ8=/fit-in/64x64/filters:strip_icc()/pic3718275.jpg', 'Azul', '2017', 2, 4.0, 45, 'Abstract Strategy', 'Introduced by the Moors, azulejos (originally white and blue ceramic tiles) were fully embraced by the Portuguese.', 'Pattern Building', '/boardgame/230802/azul'),
+(17, 'https://cf.geekdo-images.com/m_RzXpHURC0_xLkvRSR_sw__micro/img/os4ChJCgP0VGLN9WU1Ea6c4z6CE=/fit-in/64x64/filters:strip_icc()/pic3043734.jpg', 'King of Tokyo', '2011', 2, 6.0, 30, 'Dice', 'In King of Tokyo, you play mutant monsters, gigantic robots, and strange aliens.', 'Dice Rolling', '/boardgame/70323/king-tokyo'),
+(18, 'https://cf.geekdo-images.com/5CFwjd8zTcGYVUnkXh04hw__micro/img/N1VrEUaW6bmwnqbZfi2nRScwGxM=/fit-in/64x64/filters:strip_icc()/pic1176894.jpg', 'The Castles of Burgundy', '2011', 2, 4.0, 90, 'Medieval', 'The game is set in the Burgundy region of High Medieval France.', 'Dice Rolling', '/boardgame/84876/castles-burgundy'),
+(19, 'https://cf.geekdo-images.com/wLW7pFn0--20lEizEz5p3A__micro/img/HGRTd0C4poFciUwMZzBt6On1R3Y=/fit-in/64x64/filters:strip_icc()/pic2270442.jpg', 'Patchwork', '2014', 2, 2.0, 30, 'Abstract Strategy', 'In Patchwork, two players compete to build the most aesthetic (and high-scoring) patchwork quilt.', 'Tile Placement', '/boardgame/163412/patchwork'),
+(20, 'https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__micro/img/5ZaRePVhfelfofF7T_OC1e0gUCw=/fit-in/64x64/filters:strip_icc()/pic4458123.jpg', 'Wingspan', '2019', 1, 5.0, 70, 'Animals', 'Wingspan is a competitive, medium-weight, card-driven, engine-building board game.', 'Engine Building', '/boardgame/266192/wingspan'),
+(21, 'https://cf.geekdo-images.com/uSgzS-SClISqDkYRCdUq6g__micro/img/-Jb5JDZlNfelQUUHGENNZsGUelo=/fit-in/64x64/filters:strip_icc()/pic3483909.jpg', 'Dixit', '2008', 3, 6.0, 30, 'Party Game', 'One player is the storyteller for the turn and looks at the images on the 6 cards in her hand.', 'Storytelling', '/boardgame/39856/dixit'),
+(22, 'https://cf.geekdo-images.com/yd6LuatytHRhcFCxCf-EEg__micro/img/ZUrGKfXAZS4AztFRXq2W9oD6TG4=/fit-in/64x64/filters:strip_icc()/pic4459753.jpg', 'Power Grid', '2004', 2, 6.0, 120, 'Economic', 'Power Grid is the updated release of the Friedemann Friese crayon game Funkenschlag.', 'Auction', '/boardgame/2651/power-grid'),
+(23, 'https://cf.geekdo-images.com/-DOqixs8uwKUvvWPKI4f9w__micro/img/Fz_qRw_ZHI9TfZXY0b4YZ6zfT7o=/fit-in/64x64/filters:strip_icc()/pic5261714.jpg', 'Race for the Galaxy', '2007', 2, 4.0, 60, 'Science Fiction', 'In the card game Race for the Galaxy, players build galactic civilizations by playing game cards.', 'Card Drafting', '/boardgame/28143/race-galaxy'),
+(24, 'https://cf.geekdo-images.com/-hk8f8iGk_DyWyMrfiPBkg__micro/img/tWSg9ErrwvVcwaE1Kze1IsxwpGg=/fit-in/64x64/filters:strip_icc()/pic1116080.jpg', 'Lords of Waterdeep', '2012', 2, 5.0, 120, 'Fantasy', 'In this game, the players are powerful lords vying for control of this great city.', 'Worker Placement', '/boardgame/110327/lords-waterdeep'),
+(25, 'https://cf.geekdo-images.com/sZYp_3BTDGjh2unaZfZmuA__micro/img/sQyh47ClBO3d5sxPm73hMvM-JV4=/fit-in/64x64/filters:strip_icc()/pic2437871.jpg', 'Gloomhaven', '2017', 1, 4.0, 120, 'Adventure', 'Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives.', 'Campaign', '/boardgame/174430/gloomhaven'),
+(26, 'https://cf.geekdo-images.com/elmZegVZ6gp4_5izUgxGQQ__micro/img/GBNMM8kmp6Dlnel0EG4HN7lDTwo=/fit-in/64x64/filters:strip_icc()/pic1632539.jpg', 'Stone Age', '2008', 2, 4.0, 90, 'Prehistoric', 'The "Stone Age" times were hard indeed.', 'Worker Placement', '/boardgame/34635/stone-age'),
+(27, 'https://cf.geekdo-images.com/-Qer2BBPG7qGGDu6KcVDIw__micro/img/n6-sXYD6XXZoqIxq4P6AG7VPCuA=/fit-in/64x64/filters:strip_icc()/pic2452831.png', 'Pandemic Legacy: Season 1', '2015', 2, 4.0, 60, 'Medical', 'Pandemic Legacy is a co-operative campaign game, with an overarching story-arc played through 12-24 sessions.', 'Legacy', '/boardgame/161936/pandemic-legacy-season-1'),
+(28, 'https://cf.geekdo-images.com/JgAkEBUaiHOsOS94iRMs2w__micro/img/hUGdb5rZboKkpZDJpaYDV9v11w8=/fit-in/64x64/filters:strip_icc()/pic646458.jpg', 'Forbidden Island', '2010', 2, 4.0, 30, 'Adventure', 'Forbidden Island is a visually stunning cooperative board game.', 'Cooperative', '/boardgame/65244/forbidden-island'),
+(29, 'https://cf.geekdo-images.com/J-ts3MW0UhDzs621TR6cog__micro/img/eO0VqnPlr5OJmTMbIOl4QmxheRo=/fit-in/64x64/filters:strip_icc()/pic1871016.jpg', 'Munchkin', '2001', 3, 6.0, 120, 'Fantasy', 'Go down in the dungeon. Kill everything you meet. Backstab your friends and steal their stuff.', 'Take That', '/boardgame/1927/munchkin'),
+(30, 'https://cf.geekdo-images.com/lqmt2Oti_qJS65XqHcB8AA__micro/img/zIEsV9_V8WN9ylHsYXq943NEAVk=/fit-in/64x64/filters:strip_icc()/pic5146864.png', 'Betrayal at House on the Hill', '2004', 3, 6.0, 60, 'Horror', 'Betrayal at House on the Hill quickly builds suspense and excitement as players explore a haunted mansion of their own design.', 'Traitor Game', '/boardgame/10547/betrayal-house-hill');
+
+-- Insert data into Category table (same as before)
+INSERT INTO Category (category_id, category_name) VALUES
+(1, 'Strategy'),
+(2, 'Family'),
+(3, 'Party'),
+(4, 'Abstract'),
+(5, 'Thematic'),
+(6, 'War'),
+(7, 'Economic'),
+(8, 'Card Game'),
+(9, 'Adventure'),
+(10, 'Fantasy'),
+(11, 'Science Fiction'),
+(12, 'Dice'),
+(13, 'Cooperative'),
+(14, 'Puzzle'),
+(15, 'Horror'),
+(16, 'Medieval'),
+(17, 'Ancient'),
+(18, 'City Building'),
+(19, 'Farming'),
+(20, 'Negotiation'),
+(21, 'Deduction'),
+(22, 'Memory'),
+(23, 'Trains'),
+(24, 'Animals'),
+(25, 'Prehistoric'),
+(26, 'Medical'),
+(27, 'Environmental'),
+(28, 'Territory Building'),
+(29, 'Civilization'),
+(30, 'Space Exploration');
+
+-- Insert data into describes table (linking games 1-30 to categories)
+INSERT INTO describes (game_id, category_id) VALUES
+(1, 13), (1, 26),    -- Pandemic: Cooperative, Medical
+(2, 18), (2, 16),    -- Carcassonne: City Building, Medieval
+(3, 7), (3, 20),     -- Catan: Economic, Negotiation
+(4, 29), (4, 17),    -- 7 Wonders: Civilization, Ancient
+(5, 8), (5, 7),      -- Dominion: Card Game, Economic
+(6, 23), (6, 2),     -- Ticket to Ride: Trains, Family
+(7, 3), (7, 21),     -- Codenames: Party, Deduction
+(8, 11), (8, 30),    -- Terraforming Mars: Sci-Fi, Space
+(9, 17), (9, 29),    -- 7 Wonders Duel: Ancient, Civilization
+(10, 19), (10, 7),   -- Agricola: Farming, Economic
+(11, 7), (11, 18),   -- Puerto Rico: Economic, City Building
+(12, 7), (12, 8),    -- Splendor: Economic, Card Game
+(13, 11), (13, 28),  -- Scythe: Sci-Fi, Territory
+(14, 8), (14, 3),    -- Love Letter: Card Game, Party
+(15, 23), (15, 2),   -- Ticket to Ride Europe: Trains, Family
+(16, 4), (16, 14),   -- Azul: Abstract, Puzzle
+(17, 12), (17, 5),   -- King of Tokyo: Dice, Thematic
+(18, 16), (18, 7),   -- Castles of Burgundy: Medieval, Economic
+(19, 4), (19, 14),   -- Patchwork: Abstract, Puzzle
+(20, 24), (20, 2),   -- Wingspan: Animals, Family
+(21, 3), (21, 22),   -- Dixit: Party, Memory
+(22, 7), (22, 1),    -- Power Grid: Economic, Strategy
+(23, 11), (23, 30),  -- Race for Galaxy: Sci-Fi, Space
+(24, 10), (24, 1),   -- Lords of Waterdeep: Fantasy, Strategy
+(25, 9), (25, 10),   -- Gloomhaven: Adventure, Fantasy
+(26, 25), (26, 7),   -- Stone Age: Prehistoric, Economic
+(27, 13), (27, 26),  -- Pandemic Legacy: Cooperative, Medical
+(28, 13), (28, 9),   -- Forbidden Island: Cooperative, Adventure
+(29, 10), (29, 3),   -- Munchkin: Fantasy, Party
+(30, 15), (30, 9);   -- Betrayal: Horror, Adventure
+
+-- Insert sample users (same as before)
+INSERT INTO Users (user_id, password, email, username) VALUES
+(1, 'password1', 'user1@example.com', 'boardgamer1'),
+(2, 'password2', 'user2@example.com', 'gamefan2'),
+(3, 'password3', 'user3@example.com', 'strategyguru'),
+(4, 'password4', 'user4@example.com', 'dicequeen'),
+(5, 'password5', 'user5@example.com', 'cardmaster');
+
+-- Insert sample reviews (updated to use IDs 1-30)
+INSERT INTO Review (game_id, user_id, review_id, review_text) VALUES
+(1, 1, 'R1_1', 'Pandemic is a fantastic cooperative game that really makes you work together. The tension builds beautifully as diseases spread.'),
+(2, 2, 'R2_2', 'Carcassonne is a classic for a reason. Simple rules but deep strategy with the tile placement.'),
+(3, 3, 'R3_3', 'Catan is the game that got me into modern board games. Still play it regularly after all these years.'),
+(4, 4, 'R4_4', '7 Wonders is perfect when you want a civilization game that plays quickly. The drafting mechanic is brilliant.'),
+(5, 5, 'R5_5', 'Dominion created the deck-building genre and still does it best in my opinion. So many combinations to explore.'),
+(6, 1, 'R6_1', 'Ticket to Ride is my go-to game for introducing new players to the hobby. Accessible but still strategic.'),
+(7, 2, 'R7_2', 'Codenames is always a hit at parties. Simple concept but leads to lots of laughter and "aha" moments.'),
+(8, 3, 'R8_3', 'Terraforming Mars has incredible depth and replayability. The engine-building is so satisfying.'),
+(9, 4, 'R9_4', '7 Wonders Duel is one of the best 2-player games out there. Every decision feels important.'),
+(10, 5, 'R10_5', 'Agricola is brutally challenging but so rewarding when your farm finally comes together.');
