af37296cf537190bcfca2a37e9f5f18c65defd25
Lutow <118168487+Lutow@users.noreply.github.com>
2025-05-17 15:41:51 +0200

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Main from '../views/Main.vue';
 import Login from '../views/Log in.vue';
 import GameList from '../views/Games.vue';
+import Register from '../views/Register.vue';
 
 const routes = [{ path: '/login', name: 'Login', component: Login },
-{ path: '/games', name: 'Games', component: GameList }, { path: '/', name: 'Main', component: Main }
+{ path: '/games', name: 'Games', component: GameList }, { path: '/', name: 'Main', component: Main },
+  { path: '/register', name: 'Register', component: Register },
 ];
 
 
@@ -12,5 +14,6 @@ export default createRouter({
   history: createWebHistory(),
   routes,
 });
+
 
 
