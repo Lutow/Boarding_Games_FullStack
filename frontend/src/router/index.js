bfb689f7e948eae659800583d5baa24e3d123011
Lutow <118168487+Lutow@users.noreply.github.com>
2025-05-19 17:50:47 +0200

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,11 +3,13 @@ import Main from '../views/Main.vue';
 import Login from '../views/Log in.vue';
 import GameList from '../views/Games.vue';
 import Register from '../views/Register.vue';
+import GameDetail from '../views/Game.vue';
 
 
 const routes = [{ path: '/login', name: 'Login', component: Login },
 { path: '/games', name: 'Games', component: GameList }, { path: '/', name: 'Main', component: Main },
   { path: '/register', name: 'Register', component: Register },
+                {path: '/game/:id', name: 'GameDetail', component: GameDetail}
 ];
 
 
